c35cb97b53921306da7daf712ec28a1ee88c977d
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-10-20 15:12:29 -0700

    Breakout `computeCanny()`

--- main.js
+++ main.js
@@ -8,11 +8,13 @@
 import convertRgbaToGrayscale from './src/util/convert-rgba-to-grayscale.js';
 import rescaleImage from './src/util/rescale-image.js';
 import mirrorImage from './src/util/mirror-image.js';
+import computeCanny from './src/util/compute-canny.js';
 
 export default function ObjectDetector() {
   console.log(typeof convertRgbaToGrayscale);
   console.log(typeof rescaleImage);
   console.log(typeof mirrorImage);
+  console.log(typeof computeCanny);
   console.log('test');
 
   return 'test';
