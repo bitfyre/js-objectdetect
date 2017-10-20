67a6609d2ea7c53b589b01ac528e4ac3cbbbf3d0
Alex Lemanski <alex.lemanski@bitfyre.net>
2017-10-19 19:41:09 -0700

    Add main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+/**
+ * Real-time object detector based on the Viola Jones Framework.
+ * Compatible to OpenCV Haar Cascade Classifiers (stump based only).
+ *
+ * Copyright (c) 2017, Alex Lemanski
+ */
+
